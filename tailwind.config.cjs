eb1b7c24570da64b8fbe8164fc693caff78cbfb5
Piratey7007 <piratey7007@runbox.com>
2023-06-29 20:31:30 -0500

    Updated images in optimized assets folder

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
-        body: "Noto Sans",
-        display: "Eagle Lake",
+        body: ["Noto Sans", ...defaultTheme.fontFamily.sans],
+        display: ["Eagle Lake", ...defaultTheme.fontFamily.serif],
       },
     },
   },
