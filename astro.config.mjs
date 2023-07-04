87a634a85a8055743653228cae0f07324048bb3a
Piratey7007 <piratey7007@runbox.com>
2023-07-04 11:15:38 -0500

    Added prefetch

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-
 import image from "@astrojs/image";
+import compress from "astro-compress";
+
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), image()],
+  integrations: [tailwind(), image(), compress(), prefetch()],
   experimental: {
-    assets: true
-  }
+    assets: true,
+  },
 });
