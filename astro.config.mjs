ca2c27ca1d8486cca0764a2a9e8c83631f3502ab
Piratey7007 <piratey7007@runbox.com>
2023-08-11 14:43:00 -0500

    Added swup prefetching to the app

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,19 @@
-import { defineConfig, sharpImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import prefetch from "@astrojs/prefetch";
 import image from "@astrojs/image";
+import swup from "@swup/astro";
 
 export default defineConfig({
   site: "https://grizzlycafe.netlify.app",
   integrations: [
+    swup({
+      theme: false,
+      reloadScripts: false,
+      progress: true,
+      containers: ["html"],
+    }),
     prefetch(),
     tailwind(),
     image({
